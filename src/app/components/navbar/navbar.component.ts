2dd0fe32f6d64f873215a5906029b8dd0cdbc0ea
pawel <pawel.zboinski@neoteric.eu>
2019-12-09 18:26:17 +0100

    68

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean;
+  loggedInUser: string;
+  showRegister: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) {
+  }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('You are loogged out now', {
+      cssClass: 'alert-success', timeout: 3000
+    });
+    this.router.navigate(['/login']);
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,4 +22,8 @@ export class AuthService {
   getAuth() {
     return this.afAuth.authState.pipe(map(auth => auth));
   }
+
+  logout() {
+    return this.afAuth.auth.signOut();
+  }
 }
