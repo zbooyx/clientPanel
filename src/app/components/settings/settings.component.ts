689b91635c2de3c8853a70b796cdb841947c857b
pawel <pawel.zboinski@neoteric.eu>
2019-12-16 12:38:16 +0100

    72

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { SettingsService } from '../../services/settings.service';
+import { Settings } from '../../models/Settings';
+
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private flashMessage: FlashMessagesService,
+    private settingsService: SettingsService
+  ) {
   }
 
+  ngOnInit() {
+    this.settings = this.settingsService.getSettings();
+  }
+
+  onSubmit() {
+    this.settingsService.changeSettings(this.settings);
+    this.flashMessage.show('Settings saved', {
+      cssClass: 'alert-success', timeout: 3000
+    });
+  }
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -8,16 +8,22 @@ import { Settings } from '../models/Settings';
 export class SettingsService {
   settings: Settings = {
     allowRegistration: true,
-    // disableRegistration: true,
     disableBalanceOnAdd: true,
     disableBalanceOnEdit: true
   };
 
 
   constructor() {
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
   }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
